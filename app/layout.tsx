ef7aab6cd32887e57c3224aa18311d49922fc49e
techsofttest <techsofttest@gmail.com>
2026-08-14 17:12:41 +0530

    Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 
 import Header from "@/components/global/Header";
 import Footer from "@/components/global/Footer";
-import Chatbot from "@/components/global/Chatbot";
 import { getServices } from "@/app/lib/services";
 import { getContact } from "@/app/lib/contact";
 
@@ -40,7 +39,6 @@ export default async function RootLayout({
 
         {children}
 
-        <Chatbot />
         <Footer services={services} contact={contactData?.contact ?? null} />
       </body>
     </html>
